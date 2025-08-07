ba8e2a7d02ac90344ffd571383113866f5bd71f0
Anne Risgaard Hostrup <annerishos@gmail.com>
2025-08-07 10:50:35 +0200

    Opdatering inden aftale med sps

--- ConditionalStatements_Opgaver/Program.cs
+++ ConditionalStatements_Opgaver/Program.cs
@@ -16,6 +16,18 @@
             // Example IfConsistsOfUppercaseLetters:
             Console.WriteLine(IfConsistsOfUppercaseLetters("hej"));
             Console.WriteLine(IfConsistsOfUppercaseLetters("HEJ"));
+
+            // Example IfGreaterThanThirdOne:
+            Console.WriteLine(IfGreaterThanThirdOne(new int[] { 2, 7, 12 }));
+            Console.WriteLine(IfGreaterThanThirdOne(new int[] { -5, -8, 50 }));
+
+            // Example IfNumberIsEven:
+            Console.WriteLine(IfNumberIsEven(721));
+            Console.WriteLine(IfNumberIsEven(1248));
+
+            // Example IfSortedAscending:
+            Console.WriteLine(IfSortedAscending(new int[] { 3, 7, 10 }));
+            Console.WriteLine(IfSortedAscending(new int[] { 74, 62, 99 }));
         }
 
         static int AbsoluteValue(int number)
@@ -62,5 +74,49 @@
 
             return true;
         }
+
+        static bool IfGreaterThanThirdOne(int[] numbers)
+        {
+            if (numbers == null || numbers.Length != 3)
+            {
+                throw new ArgumentException("Input must be an array of exactly 3 integers.");
+            }
+
+            int first = numbers[0];
+            int second = numbers[1];
+            int third = numbers[2];
+
+            return (first + second > third) || (first * second > third);
+        }
+
+        static bool IfNumberIsEven(int number)
+        {
+            if (number % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        static bool IfSortedAscending(int[] numbers)
+        {
+            if (numbers == null || numbers.Length == 0)
+            {
+                return false;
+            }
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                if (numbers[i] > numbers[i + 1])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        static int 
     }
 }
