5b7024e18707b902fe6196419765c7df2867b294
Anne Risgaard Hostrup <annerishos@gmail.com>
2025-08-07 10:38:38 +0200

    Er godt igang med statements

--- ConditionalStatements_Opgaver/Program.cs
+++ ConditionalStatements_Opgaver/Program.cs
@@ -4,7 +4,63 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Example AbsoluteValue:
+            Console.WriteLine(AbsoluteValue(-392));
+            Console.WriteLine(AbsoluteValue(6832));
+
+            // Example DivisibleBy2Or3:
+            Console.WriteLine(DivisibleBy2Or3(15, 30));
+            Console.WriteLine(DivisibleBy2Or3(2, 90));
+            Console.WriteLine(DivisibleBy2Or3(7, 12));
+
+            // Example IfConsistsOfUppercaseLetters:
+            Console.WriteLine(IfConsistsOfUppercaseLetters("hej"));
+            Console.WriteLine(IfConsistsOfUppercaseLetters("HEJ"));
+        }
+
+        static int AbsoluteValue(int number)
+        {
+            if (number < 0)
+            {
+                return -number;
+            }
+            else
+            {
+                return number;
+            }
+        }
+
+        static int DivisibleBy2Or3(int number, int v)
+        {
+            bool numberDivisible = number % 2 == 0 || number % 3 == 0;
+            bool vDivisible = v % 2 == 0 || v % 3 == 0;
+
+            if (numberDivisible && vDivisible)
+            {
+                return number * v;
+            }
+            else
+            {
+                return number + v;
+            }
+        }
+
+        static bool IfConsistsOfUppercaseLetters(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return false;
+            }
+
+            foreach (char c in input)
+            {
+                if (!char.IsUpper(c))
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 }
